901bc7df35b927a56935f27cf719144b9bcfc628
jonny4547 <joaoapereira21@hotmail.com>
2021-03-17 18:54:43 +0000

    Add music channel to help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,6 +2,7 @@ import { Message, MessageEmbed } from "discord.js";
 import Command from "../Command";
 import commands from "../commands";
 import { botColor } from "../constants";
+import { channel as musicChannel } from "../music/musicChannel";
 
 const embed = new MessageEmbed().setColor(botColor).setTitle("Comandos");
 const adminEmbed = new MessageEmbed().setColor(botColor).setTitle("Comandos");
@@ -24,6 +25,9 @@ const help: Command = {
                     d += `\n**${command.name}**: ${command.description}`;
                 ad += `\n**${command.name}**: ${command.description}`;
             }
+
+            d += `\n\nPara tocar música usa ${musicChannel}`;
+            ad += `\n\nPara tocar música usa ${musicChannel}`;
 
             embed.setDescription(d);
             adminEmbed.setDescription(ad);
